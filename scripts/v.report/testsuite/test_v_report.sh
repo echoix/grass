d7da61cedea352649e857835b69e4ca73c0fb964
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-01-23 21:07:02 -0500

    v.report: compare script output to golden copy

--- scripts/v.report/testsuite/test_v_report.sh
+++ scripts/v.report/testsuite/test_v_report.sh
@@ -3,5 +3,15 @@
 set -e
 set -x
 
-v.report zipcodes_wake option=area units=hectares
 v.report zipcodes_wake option=area units=hectares > data/test_1_report.txt
+
+cd data
+
+for i in `ls *.txt` ; do
+    diff $i "`basename $i .txt`.ref" >> out.diff
+done
+
+CHAR_NUM=`cat out.diff | wc -c`
+
+# Return as exit status 0 in case no diffs are found
+exit $CHAR_NUM
