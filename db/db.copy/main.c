a0ca9996a526bd10ef3ae5ad0874997d4c7776b1
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-03-06 04:10:12 +0000

    Add a formatting errors in db.copy

--- db/db.copy/main.c
+++ db/db.copy/main.c
@@ -18,10 +18,10 @@
 #include <grass/dbmi.h>
 #include <grass/glocale.h>
 
-int main(int argc, char **argv)
-{
+int main(int argc,     char **argv)
+ {
     int ret;
-    struct Option *from_driver, *from_database, *from_table;
+    struct Option * from_driver, *from_database, *from_table;
     struct Option *to_driver, *to_database, *to_table;
     struct Option *where, *select;
     struct GModule *module;
