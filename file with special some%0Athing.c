28681cd4ee217e2efafcb2ee29391dd509b90453
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-03-06 05:26:11 +0000

    Add a file with special characters in the name

---
+++ file with special some%0Athing.c
@@ -0,0 +1,21 @@
+/****************************************************************************
+ *
+ * MODULE:       db.copy
+ * AUTHOR(S):    Radim Blazek <radim.blazek gmail.com> (original contributor)
+ *               Glynn Clements <glynn gclements.plus.com>,
+ *               Markus Neteler <neteler itc.it>
+ * PURPOSE:      copy a table
+ * COPYRIGHT:    (C) 2003-2006 by the GRASS Development Team
+ *
+ *               This program is free software under the GNU General Public
+ *               License (>=v2). Read the file COPYING that comes with GRASS
+ *               for details.
+ *
+ *****************************************************************************/
+
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    int ret;
+}
