2bbe0e5bde8e0cb317f7cbada4693d66bd57922c
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-03-07 17:59:12 -0500

    CI(Travis): Add debugging output at packages install

--- .travis/linux.install.sh
+++ .travis/linux.install.sh
@@ -2,8 +2,17 @@
 # Author: Ivan Mincik, ivan.mincik@gmail.com
 
 set -e
+set -x
 
 sudo apt-get update -y
     sudo apt-get install -y wget git gawk findutils
     xargs -a <(awk '! /^ *(#|$)/' ".github/workflows/apt.txt") -r -- \
         sudo apt-get install -y --no-install-recommends --no-install-suggests
+
+# https://github.com/qgis/QGIS/issues/56285
+dpkg -l | grep pdal
+apt rdepends libpdal-util13
+apt depends libpdal-base13
+apt depends libpdal-base16
+apt policy pdal
+dpkg -l | grep -E "pdal16|pdal.*2\.5"
