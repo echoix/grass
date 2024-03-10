a6a9a8573c93e984e35b913b147bffc641d79e30
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-03-10 14:54:11 -0400

    Debugging: Show output of dpkg -l --selected-only before and after install

--- .travis/linux.install.sh
+++ .travis/linux.install.sh
@@ -3,9 +3,10 @@
 
 set -e
 set -x
-
+dpkg -l
+dpkg -l --selected-only
 sudo apt-get update -y
-    sudo apt-get install -y wget git gawk findutils
+    sudo apt-get install -y wget git gawk findutils pkg-config libpdal-plugins
     xargs -a <(awk '! /^ *(#|$)/' ".github/workflows/apt.txt") -r -- \
         sudo apt-get install -y --no-install-recommends --no-install-suggests
 
@@ -17,3 +18,5 @@ apt depends libpdal16
 apt depends libpdal-dev
 apt policy libpdal-dev
 apt policy pdal
+dpkg -l
+dpkg -l --selected-only
