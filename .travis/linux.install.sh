73b583565e7b039e8ea07b26b7bb39c699ff5d56
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-03-21 17:23:24 -0400

    CI(Travis): Do not fail on listing dependencies of inexistant packages (for debugging)

--- .travis/linux.install.sh
+++ .travis/linux.install.sh
@@ -12,11 +12,11 @@ sudo apt-get update -y
 
 # https://github.com/qgis/QGIS/issues/56285
 dpkg -l | grep pdal
-apt rdepends libpdal-util13
-apt depends libpdal-base13
-apt depends libpdal16
-apt depends libpdal-dev
-apt policy libpdal-dev
-apt policy pdal
+apt rdepends libpdal-util13 || echo "not found"
+apt depends libpdal-base13 || echo "not found"
+apt depends libpdal16 || echo "not found"
+apt depends libpdal-dev || echo "not found"
+apt policy libpdal-dev || echo "not found"
+apt policy pdal || echo "not found"
 dpkg -l
 dpkg -l --selected-only
