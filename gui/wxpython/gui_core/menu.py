3b8a23ed84edc6327290c5502249cc104db3f52d
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-03-29 23:44:36 +0000

    python: Format python files with Black 24.3.0

--- gui/wxpython/gui_core/menu.py
+++ gui/wxpython/gui_core/menu.py
@@ -20,6 +20,7 @@ This program is free software under the GNU General Public License
 @author Vaclav Petras <wenzeslaus gmail.com> (menu customization)
 @author Tomas Zigo <tomas.zigo slovanet.sk> RecentFilesMenu
 """
+
 import re
 import os
 import wx
