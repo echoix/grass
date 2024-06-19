41b59beb56c350aa07340f5f41c30a2a13a9a83c
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-06-19 16:50:16 +0000

    pytest: Use pytest.importorskip

--- python/grass/jupyter/tests/timeseriesmap_test.py
+++ python/grass/jupyter/tests/timeseriesmap_test.py
@@ -3,15 +3,11 @@
 from pathlib import Path
 import pytest
 
-try:
-    import IPython
-except ImportError:
-    IPython = None
 
-try:
-    import ipywidgets
-except ImportError:
-    ipywidgets = None
+IPython = pytest.importorskip("IPython", reason="IPython package not available")
+ipywidgets = pytest.importorskip(
+    "ipywidgets", reason="ipywidgets package not available"
+)
 
 import grass.jupyter as gj
 from grass.jupyter.timeseriesmap import collect_layers, fill_none_values
@@ -71,9 +67,15 @@ def test_render_layers(space_time_raster_dataset, fill_gaps):
 
 
 @pytest.mark.needs_solo_run
-@pytest.mark.skipif(IPython is None, reason="IPython package not available")
-@pytest.mark.skipif(ipywidgets is None, reason="ipywidgets package not available")
 def test_save(space_time_raster_dataset, tmp_path):
+    """Test returns from animate and time_slider are correct object types"""
+    img = gj.TimeSeriesMap()
+    img.add_raster_series(space_time_raster_dataset.name)
+    gif_file = img.save(tmp_path / "image.gif")
+    assert Path(gif_file).is_file()
+
+@pytest.mark.needs_solo_run
+def test_save1(space_time_raster_dataset, tmp_path):
     """Test returns from animate and time_slider are correct object types"""
     img = gj.TimeSeriesMap()
     img.add_raster_series(space_time_raster_dataset.name)
