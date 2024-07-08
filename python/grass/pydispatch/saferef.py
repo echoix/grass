f53d50d578ff4b94a8abdcafd2266e5c0049c091
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-07-08 02:34:27 +0000

    style: Fix unreliable-callable-check (B004)

--- python/grass/pydispatch/saferef.py
+++ python/grass/pydispatch/saferef.py
@@ -123,7 +123,7 @@ class BoundMethodWeakref:
                 pass
             for function in methods:
                 try:
-                    if hasattr(function, "__call__"):
+                    if callable(function):
                         function(self)
                 except Exception as e:
                     try:
