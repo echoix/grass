f8eb00902e67482495dcbc405781ad75c380d7cc
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-07-14 18:44:08 +0000

    Squashed commit of the following:
    commit f3e7a4f3b1e17b8f19d25d0332134bfa0ec3400b
    Author: Edouard Choinière <27212526+echoix@users.noreply.github.com>
    Date:   Sun Jul 14 17:00:45 2024 +0000
    
        style: Fix all flake8-gettext (INT) errors (INT001, INT002, INT003)
    
        Ruff rules:
        https://docs.astral.sh/ruff/rules/f-string-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/format-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/printf-in-get-text-func-call/
    
    commit 3cd99a017c59c68bf7d41dd3b4d3907877a18c15
    Author: Edouard Choinière <27212526+echoix@users.noreply.github.com>
    Date:   Sun Jul 14 15:40:24 2024 +0000
    
        style: Fix all flake8-gettext (INT) errors (INT001, INT002, INT003)
    
        Ruff rules:
        https://docs.astral.sh/ruff/rules/f-string-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/format-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/printf-in-get-text-func-call/
    
    commit 0a9de8c452b65ca9957c2a6b84a4e02cb04fba12
    Author: Edouard Choinière <27212526+echoix@users.noreply.github.com>
    Date:   Sun Jul 14 15:26:57 2024 +0000
    
        style: Fix all flake8-gettext (INT) errors (INT001, INT002, INT003)
    
        Ruff rules:
        https://docs.astral.sh/ruff/rules/f-string-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/format-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/printf-in-get-text-func-call/
    
    commit 50cb143cb1d066c0a083287378efc84cdbd71ae0
    Author: Edouard Choinière <27212526+echoix@users.noreply.github.com>
    Date:   Sun Jul 14 14:54:50 2024 +0000
    
        style: Fix all flake8-gettext (INT) errors (INT001, INT002, INT003)
    
        Ruff rules:
        https://docs.astral.sh/ruff/rules/f-string-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/format-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/printf-in-get-text-func-call/
    
        For stds_export.py, the strings were normalized with placeholders reducing almost-identical strings to identical strings
    
    commit 9a109d2c1f393b2b08943128e5fe60c2dc085426
    Author: Edouard Choinière <27212526+echoix@users.noreply.github.com>
    Date:   Sun Jul 14 14:35:50 2024 +0000
    
        style: Fix all flake8-gettext (INT) errors (INT001, INT002, INT003)
    
        Ruff rules:
        https://docs.astral.sh/ruff/rules/f-string-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/format-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/printf-in-get-text-func-call/
    
    commit bfc04b1cb46a86a37663904565bbaba34fd78f06
    Author: Edouard Choinière <27212526+echoix@users.noreply.github.com>
    Date:   Sun Jul 14 14:09:29 2024 +0000
    
        style: Fix all flake8-gettext (INT) errors (INT001, INT002, INT003)
    
        Ruff rules:
        https://docs.astral.sh/ruff/rules/f-string-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/format-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/printf-in-get-text-func-call/
    
    commit be04e4bba0c48209a6d8b7017e2af0802f529e78
    Author: Edouard Choinière <27212526+echoix@users.noreply.github.com>
    Date:   Sun Jul 14 13:44:55 2024 +0000
    
        style: Fix all flake8-gettext (INT) errors (INT001, INT002, INT003)
    
        Ruff rules:
        https://docs.astral.sh/ruff/rules/f-string-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/format-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/printf-in-get-text-func-call/
    
    commit 916961796e69a7fd49256f545a09ef62856a16c0
    Author: Edouard Choinière <27212526+echoix@users.noreply.github.com>
    Date:   Sun Jul 14 13:04:01 2024 +0000
    
        style: Fix all flake8-gettext (INT) errors (INT001, INT002, INT003)
    
        Ruff rules:
        https://docs.astral.sh/ruff/rules/f-string-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/format-in-get-text-func-call/
        https://docs.astral.sh/ruff/rules/printf-in-get-text-func-call/

--- python/grass/temporal/core.py
+++ python/grass/temporal/core.py
@@ -768,12 +768,12 @@ def init(raise_fatal_error=False, skip_db_version_check=False):
                 "Temporal database version mismatch detected.\n{backup}"
                 "Supported temporal database version is: {tdb}\n"
                 "Your existing temporal database version: {ctdb}\n"
-                "Current temporal database info: {info}".format(
-                    backup=backup_howto,
-                    tdb=tgis_db_version,
-                    ctdb=tgis_db_version_meta,
-                    info=get_database_info_string(),
-                )
+                "Current temporal database info: {info}"
+            ).format(
+                backup=backup_howto,
+                tdb=tgis_db_version,
+                ctdb=tgis_db_version_meta,
+                info=get_database_info_string(),
             )
 
             if tgis_db_version_meta == 2 and tgis_db_version == 3:
@@ -783,8 +783,8 @@ def init(raise_fatal_error=False, skip_db_version_check=False):
                 msgr.fatal(
                     _(
                         "The format of your actual temporal database is "
-                        "not supported any more. {m}".format(m=message)
-                    )
+                        "not supported any more. {m}"
+                    ).format(m=message)
                 )
 
         return
@@ -879,7 +879,7 @@ def create_temporal_database(dbif):
     stvds_tables_sql = stds_tables_template_sql.replace("STDS", "stvds")
     str3ds_tables_sql = stds_tables_template_sql.replace("STDS", "str3ds")
 
-    msgr.message(_("Creating temporal database: %s" % (str(tgis_database_string))))
+    msgr.message(_("Creating temporal database: %s") % (str(tgis_database_string)))
 
     if tgis_backend == "sqlite":
         # We need to create the sqlite3 database path if it does not exist
@@ -892,8 +892,9 @@ def create_temporal_database(dbif):
                     _(
                         "Unable to create SQLite temporal database\n"
                         "Exception: %s\nPlease use t.connect to set a "
-                        "read- and writable temporal database path" % (e)
+                        "read- and writable temporal database path"
                     )
+                    % (e)
                 )
 
         # Set up the trigger that takes care of
@@ -1528,7 +1529,7 @@ class DBConnection:
         except:
             if connected:
                 self.close()
-            self.msgr.error(_("Unable to execute :\n %(sql)s" % {"sql": statement}))
+            self.msgr.error(_("Unable to execute :\n %(sql)s") % {"sql": statement})
             raise
 
         if connected:
@@ -1572,7 +1573,7 @@ class DBConnection:
             if connected:
                 self.close()
             self.msgr.error(
-                _("Unable to execute transaction:\n %(sql)s" % {"sql": statement})
+                _("Unable to execute transaction:\n %(sql)s") % {"sql": statement}
             )
             raise
 
