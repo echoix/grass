dc022d4334e2766ab0c78a8aab61416ba023d3dc
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-07-21 21:02:05 +0000

    DEBUG: printenv | sort in test_thorough.sh

--- .github/workflows/test_thorough.sh
+++ .github/workflows/test_thorough.sh
@@ -9,6 +9,8 @@ else
   PYTHON="${PYTHON}"
 fi
 
+printenv | sort
+
 grass --tmp-project XY --exec \
     g.download.location url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_full_v2alpha2.tar.gz path=$HOME
 
