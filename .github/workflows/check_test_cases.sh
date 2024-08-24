da0d5cb439b71413d248687c986f313b48196e3f
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-08-24 02:33:02 +0000

    DEBUG: Edit run script and call

--- .github/workflows/check_test_cases.sh
+++ .github/workflows/check_test_cases.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+echo "::group::Compile"
+echo "Inside group"
+source ./.github/workflows/macos_install.sh $HOME/install || true
+# source ./.github/workflows/macos_install.sh $HOME/install || echo "::endgroup::" && exit 125
+echo "::endgroup::"
 
+echo "::group::Test"
 export PYTHONPATH=$(grass --config python_path):$PYTHONPATH
 export LD_LIBRARY_PATH=$(grass --config path)/lib:$LD_LIBRARY_PATH
 pytest --verbose --color=yes --durations=0 --durations-min=0.5 \
             -ra . \
             -m 'needs_solo_run'
+echo "::endgroup::"
