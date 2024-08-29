5297d2a4457533d8f70194d60f786158c601eb57
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-08-29 19:44:24 -0400

    Update package.sh update dolls

--- mswindows/osgeo4w/package.sh
+++ mswindows/osgeo4w/package.sh
@@ -110,29 +110,20 @@ DLLS="
 	/mingw64/bin/zlib1.dll
 	/mingw64/bin/libbz2-1.dll
 	/mingw64/bin/libiconv-2.dll
-	/mingw64/bin/libfontconfig-1.dll
 	/mingw64/bin/libgfortran-5.dll
-	/mingw64/bin/libbrotlidec.dll
-	/mingw64/bin/libbrotlicommon.dll
 	/mingw64/bin/libintl-8.dll
 	/mingw64/bin/libsystre-0.dll
 	/mingw64/bin/libtre-5.dll
 	/mingw64/bin/libwinpthread-1.dll
-	/mingw64/bin/libcairo-2.dll
-	/mingw64/bin/libpixman-1-0.dll
-	/mingw64/bin/libpng16-16.dll
-	/mingw64/bin/libfreetype-6.dll
-	/mingw64/bin/libharfbuzz-0.dll
-	/mingw64/bin/libglib-2.0-0.dll
+    /mingw64/bin/libomp.dll
 	/mingw64/bin/libgomp-1.dll
-	/mingw64/bin/libgraphite2.dll
-	/mingw64/bin/libpcre-1.dll
 	/mingw64/bin/libstdc++-6.dll
 	/mingw64/bin/libgcc_s_seh-1.dll
 	/mingw64/bin/libfftw3-3.dll
 	/mingw64/bin/libblas.dll
 	/mingw64/bin/liblapack.dll
 	/mingw64/bin/libquadmath-0.dll
+    /mingw64/bin/libreadline8.dll
 "
 
 if ! [ -f mswindows/osgeo4w/configure-stamp ]; then
