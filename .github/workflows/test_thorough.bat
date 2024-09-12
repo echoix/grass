01db5e9b36cca6bb0cdba65d129df01af3d9735b
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-09-12 16:23:18 -0400

    Update grass test configuration file name in test_thorough.bat.

--- .github/workflows/test_thorough.bat
+++ .github/workflows/test_thorough.bat
@@ -4,4 +4,4 @@ set python=%2
 
 call %grass% --tmp-project XY --exec g.download.project url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_full_v2alpha2.tar.gz path=%USERPROFILE%
 @echo on
-call %grass% --tmp-project XY --exec %python% -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 80 --config .gunittest2.cfg
+call %grass% --tmp-project XY --exec %python% -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 80 --config .gunittest.cfg
