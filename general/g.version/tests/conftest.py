12d74bf1f915788d39511b6b8f851c7217a025cf
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-10-05 17:18:05 +0000

    g.version: Delete GISBASE env var before calling gs.create_project

--- general/g.version/tests/conftest.py
+++ general/g.version/tests/conftest.py
@@ -4,10 +4,11 @@ import grass.script as gs
 
 
 @pytest.fixture
-def session(tmp_path_factory):
+def session(tmp_path_factory, monkeypatch):
     """Set up a GRASS session for the tests."""
     tmp_path = tmp_path_factory.mktemp("grass_session")
     project = "test_project"
+    monkeypatch.delenv("GISBASE", raising=False)
 
     # Create a test location
     gs.create_project(tmp_path, project)
