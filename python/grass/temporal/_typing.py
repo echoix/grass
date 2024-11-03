91ab56b103229a72c19956b2ac7bdba0e205a14c
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-11-03 15:58:07 +0000

    grass.temporal: Other typing trials

--- python/grass/temporal/_typing.py
+++ python/grass/temporal/_typing.py
@@ -15,6 +15,10 @@ class Raster3DT(AnyRasterT):
     pass
 
 
+class VectorT:
+    pass
+
+
 # class RasterT:
 #     pass
 
@@ -23,20 +27,23 @@ class Raster3DT(AnyRasterT):
 #     pass
 
 
-class VectorT:
-    pass
-
-
 # AnyRasterT = RasterT | Raster3DT
 # AnyRasterT = Union[RasterT, Raster3DT]
 # RTT = TypeVar("RTT", AnyRasterT)
+# RTT = TypeVar("RTT", bound=AnyRasterT)
 RTT = TypeVar("RTT", bound=AnyRasterT)
+# RTT = TypeVar("RTT", bound=AnyRasterT, contravariant=True)
+# RTT = TypeVar("RTT", bound=AnyRasterT, covariant=True)
 # RTT = TypeVar("RTT", RasterT, Raster3DT)
 # RTT = TypeVar("RTT", bound=AnyRasterT)
 # TT = TypeVar("TT", RasterT, Raster3DT, VectorT)
 # TT = TypeVar("TT", bound=AnyRasterT | VectorT)
 # TT = TypeVar("TT", RTT, VectorT)
-TT = TypeVar("TT", RasterT, Raster3DT, VectorT)
+# TT = TypeVar("TT", AnyRasterT, RasterT, Raster3DT, VectorT, covariant=True)
+TT = TypeVar("TT", AnyRasterT, RasterT, Raster3DT, VectorT)
+# TT = TypeVar("TT", AnyRasterT, RasterT, Raster3DT, VectorT, contravariant=True)
+# TT = TypeVar("TT", AnyRasterT, RasterT, Raster3DT, VectorT)
+# TT = TypeVar("TT", RasterT, Raster3DT, VectorT)
 # TT = TypeVar("TT", AnyRasterT, VectorT)
 # TT = TypeVar("TT", AnyRasterT, VectorT, contravariant=True)
 # TT = TypeVar("TT", AnyRasterT, VectorT, covariant=True)
@@ -46,7 +53,21 @@ class SpaceTimeT(Generic[TT]):
     pass
 
 
+class AnySpaceTimeT(SpaceTimeT[TT], Generic[TT]):
+    pass
+
+# class AnySpaceTimeT(SpaceTimeT[TT]):
+#     pass
+
+
+SpaceTimeTT = SpaceTimeT[TT]
+STT2 = TypeVar("STT2", bound=SpaceTimeTT)
 STT = TypeVar("STT", bound=SpaceTimeT)
+STT3 = TypeVar("STT3", bound=AnySpaceTimeT)
+# STT = TypeVar("STT", bound=SpaceTimeT, contravariant=True)
+# STT = TypeVar("STT", bound=SpaceTimeT, covariant=True)
+# SpaceTimeTT = SpaceTimeT[TT]
+# STT = TypeVar("STT", bound=SpaceTimeTT)
 # AnyTTST = Union[TT, STT]
 # AnyTTST = TypeVar("AnyTTST", bound=TT|STT)
 # AnyTTST = TypeVar("AnyTTST", bound=Union[TT, STT])
@@ -60,7 +81,48 @@ AnyTTST2 = TypeVar(
     SpaceTimeT[VectorT],
 )
 # AnyTTST = TypeVar("AnyTTST", RasterT, Raster3DT, VectorT, SpaceTimeT)
-AnyTTST = TypeVar("AnyTTST", RasterT, Raster3DT, VectorT, SpaceTimeT)
+AnyTTST = TypeVar("AnyTTST", AnyRasterT, RasterT, Raster3DT, VectorT, SpaceTimeT)
+AnyTTST3 = TypeVar(
+    "AnyTTST3",
+    AnyRasterT,
+    RasterT,
+    Raster3DT,
+    VectorT,
+    SpaceTimeT,
+    SpaceTimeT[AnyRasterT],
+    SpaceTimeT[RasterT],
+    SpaceTimeT[Raster3DT],
+    SpaceTimeT[VectorT],
+)
+AnyTTST4 = TypeVar(
+    "AnyTTST4",
+    AnyRasterT,
+    RasterT,
+    Raster3DT,
+    VectorT,
+    AnySpaceTimeT,
+    SpaceTimeT,
+    SpaceTimeT[AnyRasterT],
+    SpaceTimeT[RasterT],
+    SpaceTimeT[Raster3DT],
+    SpaceTimeT[VectorT],
+)
+# AnyTTST = TypeVar(
+#     "AnyTTST",
+#     AnyRasterT,
+#     RasterT,
+#     Raster3DT,
+#     VectorT,
+#     SpaceTimeT[AnyRasterT],
+#     SpaceTimeT[RasterT],
+#     SpaceTimeT[Raster3DT],
+#     SpaceTimeT[VectorT],
+# )
+# AnyTTST = TypeVar("AnyTTST", AnyRasterT, RasterT, Raster3DT, VectorT, SpaceTimeT)
+# AnyTTST = TypeVar("AnyTTST", AnyRasterT, RasterT, Raster3DT, VectorT, SpaceTimeTT)
+# AnyTTST = TypeVar("AnyTTST", AnyRasterT, RasterT, Raster3DT, VectorT, SpaceTimeT,
+#  SpaceTimeTT)
+# AnyTTST = TypeVar("AnyTTST", RasterT, Raster3DT, VectorT, SpaceTimeT)
 # AnyTTST = TypeVar("AnyTTST",RasterT,Raster3DT,VectorT,SpaceTimeT, contravariant=True)
 # AnyTTST = TypeVar("AnyTTST", AnyRasterT, VectorT, SpaceTimeT, contravariant=True)
 # AnyTTST = TypeVar("AnyTTST", AnyRasterT, VectorT, SpaceTimeT, covariant=True)
