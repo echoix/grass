c115d09b0fbe32bc5a55be2be41b8d966321aa63
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-11-24 21:27:30 +0000

    mswindows: Adapt vswhere example into bash to add the folder containing dumpbin to PATH
    https://github.com/microsoft/vswhere/wiki/Find-VC

--- mswindows/osgeo4w/mklibs.sh
+++ mswindows/osgeo4w/mklibs.sh
@@ -3,8 +3,26 @@
 set -e
 
 if [ "$CI" ] ; then
+	HostArch="x64"
+	Arch="x64"
+	vctoolsBinPath='C:/Program Files (x86)\Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/'
+	echo "Calling vswhere"
+	vswhere -? || true
+	# Adapted the usage examples of vswhere for bash, and our specific needs: https://github.com/microsoft/vswhere/wiki/Find-VC
+	installDir="$(vswhere -latest -products "*" -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath)"
+	echo "installDir is: $installDir"
+	if [ -d "$installDir" ]; then
+		versionFilePath="${installDir}/VC/Auxiliary/Build/Microsoft.VCToolsVersion.default.txt"
+		if [ -f "$versionFilePath" ]; then
+			versionContents=$(cat "${versionFilePath}")
+			echo "Default VCToolsVersion is: '$versionContents'"
+			vctoolsBinPath="${installDir}/VC/Tools/MSVC/${versionContents}/bin/Host${HostArch}/${Arch}/"
+		fi
+	fi
+	vctoolsBinPathMsys="$(cygpath -ua "$vctoolsBinPath")"
 	# dumpbin in GH actions moved to sub-directory
-	export PATH="$PATH:$(cygpath -ua 'C:/Program Files (x86)\Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/')"
+	# export PATH="$PATH:$(cygpath -ua 'C:/Program Files (x86)\Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/')"
+	export PATH="$PATH:${vctoolsBinPathMsys}"
 	# dumpbin in GH actions does not support options starting with "-"
 	DUMPBIN_EXPORT="/EXPORTS"
 else
