bab6d3a6af9a76d62a1fcef0393f4f79ce90edbb
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-11-24 14:06:50 +0000

    pygrass: Split hanging modules/interface test into separate files to find which part is problematic

--- python/grass/pygrass/modules/interface/testsuite/test_modules_with_metaclass.py
+++ python/grass/pygrass/modules/interface/testsuite/test_modules_with_metaclass.py
@@ -5,13 +5,11 @@ Created on Tue Jun 24 09:43:53 2014
 """
 
 from fnmatch import fnmatch
-from io import BytesIO
 
 from grass.gunittest.case import TestCase
 from grass.gunittest.main import test
 
 from grass.script.core import get_commands
-from grass.exceptions import ParameterError
 from grass.pygrass.modules.interface import Module
 
 
@@ -41,29 +39,6 @@ class ModulesMeta(type):
 
 class TestModules(TestCase, metaclass=ModulesMeta):
     pass
-
-
-class TestModulesPickability(TestCase):
-    def test_rsun(self):
-        """Test if a Module instance is pickable"""
-        import pickle
-
-        out = BytesIO()
-        pickle.dump(Module("r.sun"), out)
-        out.close()
-
-
-class TestModulesCheck(TestCase):
-    def test_flags_with_suppress_required(self):
-        """Test if flags with suppress required are handle correctly"""
-        gextension = Module("g.extension")
-        # check if raise an error if required parameter are missing
-        with self.assertRaises(ParameterError):
-            gextension.check()
-
-        # check if the flag suppress the required parameters
-        gextension.flags.a = True
-        self.assertIsNone(gextension.check())
 
 
 if __name__ == "__main__":
