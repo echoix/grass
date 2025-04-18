e63d5b77e9dee3ba8caef760fd615c4cc901be44
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-04-18 19:30:12 -0400

    Remove redundant flags from CPPFLAGS in macOS script

--- .github/workflows/macos_install.sh
+++ .github/workflows/macos_install.sh
@@ -61,7 +61,7 @@ CONFIGURE_FLAGS="\
 
 export CFLAGS="-O2 -pipe -ffp-contract=off -arch ${CONDA_ARCH} -DGL_SILENCE_DEPRECATION -Wall -Wextra -Wpedantic -Wvla -fprofile-instr-generate -fcoverage-mapping"
 export CXXFLAGS="-O2 -pipe -ffp-contract=off -stdlib=libc++ -arch ${CONDA_ARCH} -Wall -Wextra -Wpedantic -fprofile-instr-generate -fcoverage-mapping"
-export CPPFLAGS="-isystem${CONDA_PREFIX}/include -fprofile-instr-generate -fcoverage-mapping"
+export CPPFLAGS="-isystem${CONDA_PREFIX}/include"
 
 ./configure $CONFIGURE_FLAGS
 
