0faba3408695b2818fe6f9f1993a8bc59c2b5043
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-04-21 23:55:16 +0000

    CI(macOS): Remove -v from LDFLAGS

--- .github/workflows/macos_install.sh
+++ .github/workflows/macos_install.sh
@@ -69,7 +69,7 @@ export CPPFLAGS="-isystem${CONDA_PREFIX}/include"
 export CFLAGS="$EXTRA_COV_FLAGS $CFLAGS"
 export CXXFLAGS="$EXTRA_COV_FLAGS $CXXFLAGS"
 
-LDFLAGS="-v -fprofile-instr-generate -fcoverage-mapping $LDFLAGS" ./configure $CONFIGURE_FLAGS
+LDFLAGS="-fprofile-instr-generate -fcoverage-mapping $LDFLAGS" ./configure $CONFIGURE_FLAGS
 
 EXEMPT=""
 make -j$(sysctl -n hw.ncpu) CFLAGS="$CFLAGS -Werror $EXEMPT" \
