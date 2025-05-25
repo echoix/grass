4013498cff7d47ab03d8fefe513e199abf462b3d
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-05-25 10:37:12 -0400

    Append LDFLAGS in make

--- .github/workflows/macos_install.sh
+++ .github/workflows/macos_install.sh
@@ -73,6 +73,6 @@ LDFLAGS="-fprofile-instr-generate -fcoverage-mapping $LDFLAGS" ./configure $CONF
 
 EXEMPT=""
 make -j$(sysctl -n hw.ncpu) CFLAGS="$CFLAGS -Werror $EXEMPT" \
-  CXXFLAGS="$CXXFLAGS -Werror $EXEMPT" LDFLAGS="-fprofile-instr-generate -fcoverage-mapping $LDFLAGS"
+  CXXFLAGS="$CXXFLAGS -Werror $EXEMPT" LDFLAGS=" $LDFLAGS -fprofile-instr-generate "
 
 make install
