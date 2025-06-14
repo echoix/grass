45d8dbe4001746ed1547da2ffa57f1672095a829
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-06-14 23:11:12 +0000

    docs: Insert import paths in conf.py

--- python/grass/docs/conf.py
+++ python/grass/docs/conf.py
@@ -3,6 +3,73 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import string
+import sys
+
+# -- Additional Paths -- -----------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+if not os.getenv("GISBASE"):
+    import logging
+
+    logging.fatal("GISBASE not defined")
+    sys.exit("GISBASE not defined")
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.environ["GISBASE"], "etc", "python", "grass")),
+)
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(os.environ["GISBASE"], "etc", "python", "grass", "ctypes")
+    ),
+)
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(os.environ["GISBASE"], "etc", "python", "grass", "exceptions")
+    ),
+)
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(os.environ["GISBASE"], "etc", "python", "grass", "gunittest")
+    ),
+)
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(os.environ["GISBASE"], "etc", "python", "grass", "imaging")
+    ),
+)
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(os.environ["GISBASE"], "etc", "python", "grass", "pydispatch")
+    ),
+)
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(os.environ["GISBASE"], "etc", "python", "grass", "pygrass")
+    ),
+)
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(os.environ["GISBASE"], "etc", "python", "grass", "script")
+    ),
+)
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(os.environ["GISBASE"], "etc", "python", "grass", "temporal")
+    ),
+)
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
