10e6200389a7299f8c5952984cfb3eef53797195
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2026-07-04 12:45:28 +0000

    grass.tools: Typing annotation for init's `__getattr__` overloads

--- python/grass/tools/__init__.py
+++ python/grass/tools/__init__.py
@@ -1,4 +1,25 @@
-def __getattr__(name):
+from __future__ import annotations
+from tools.session_tools import Tools
+from tools.session_tools import ToolError
+from typing import TYPE_CHECKING, overload
+
+if TYPE_CHECKING:
+    from typing import Literal
+    from .session_tools import Tools
+    from .session_tools import ToolError
+
+
+@overload
+def __getattr__(name: Literal["Tools"]) -> type[Tools]:
+    pass
+
+
+@overload
+def __getattr__(name: Literal["ToolError"]) -> type[ToolError]:
+    pass
+
+
+def __getattr__(name) -> type[ToolError | Tools]:
     if name == "Tools":
         from .session_tools import Tools
 
